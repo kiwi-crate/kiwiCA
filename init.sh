83a8bf1f6003ba311ba95010a7e8fda435d692d1
fluffehGit <cst.petre@zoho.eu>
2022-09-26 15:55:12 +0300

    +moved pem and privkey to bind-mount

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 ROOTCA_MOUNT_LOCATION="/opt/ca-crate/rootCA"
-source ${2}
+source ${ROOTCA_MOUNT_LOCATION}/rootCA_pem_subject
 
 if [ ! -e ${ROOTCA_MOUNT_LOCATION}/rootCA.key ]; then
-    openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -aes256 -out ${ROOTCA_MOUNT_LOCATION}/rootCA.key -pass file:${1}
-    openssl req -x509 -new -key ${ROOTCA_MOUNT_LOCATION}/rootCA.key -sha256 -days 365 -out ${ROOTCA_MOUNT_LOCATION}/rootCA.pem -passin file:${1} -subj ${PEM_SUBJ}
+    openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -aes256 -out ${ROOTCA_MOUNT_LOCATION}/rootCA.key -pass file:${ROOTCA_MOUNT_LOCATION}/rootCA_pkey_password
+    openssl req -x509 -new -key ${ROOTCA_MOUNT_LOCATION}/rootCA.key -sha256 -days 365 -out ${ROOTCA_MOUNT_LOCATION}/rootCA.pem -passin file:${ROOTCA_MOUNT_LOCATION}/rootCA_pkey_password -subj ${ROOTCA_PEM_SUBJECT}
 else
     echo -e "Private key already exists!\nRootCA certificate generation interrupted!";
     exit 10;
