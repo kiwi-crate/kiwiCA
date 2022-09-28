ca8ad6a3254725198da5970f6eca9654c322781a
fluffehGit <cst.petre@zoho.eu>
2022-09-28 14:00:09 +0300

    +openssl config

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,15 @@ ENV TZ="Europe/Brussels"
 # Create base folders for CA
 RUN mkdir /opt/ca-crate \
           /opt/ca-crate/certs \
-          /opt/ca-crate/CRL \
+          /opt/ca-crate/crl \
+          /opt/ca-crate/newcerts \
           /opt/ca-crate/private \
           /opt/ca-crate/CSRs
+
+COPY ./openssl.cnf /etc/ssl/openssl.cnf
+
+RUN touch /opt/ca-crate/index.txt && \
+    echo "1000" > /opt/ca-crate/serial && \
+    chmod 700 /opt/ca-crate/private
 
 COPY ./init.sh /root
--- init.sh
+++ init.sh
@@ -4,9 +4,28 @@ ROOTCA_MOUNT_LOCATION="/opt/ca-crate/rootCA"
 source ${ROOTCA_MOUNT_LOCATION}/rootCA_pem_subject
 
 if [ ! -e ${ROOTCA_MOUNT_LOCATION}/rootCA.key ]; then
-    openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -aes256 -out ${ROOTCA_MOUNT_LOCATION}/rootCA.key -pass file:${ROOTCA_MOUNT_LOCATION}/rootCA_pkey_password
-    openssl req -x509 -new -key ${ROOTCA_MOUNT_LOCATION}/rootCA.key -sha256 -days 365 -out ${ROOTCA_MOUNT_LOCATION}/rootCA.pem -passin file:${ROOTCA_MOUNT_LOCATION}/rootCA_pkey_password -subj ${ROOTCA_PEM_SUBJECT}
+    openssl genpkey -algorithm RSA                                      \
+                    -pkeyopt rsa_keygen_bits:4096                       \
+                    -aes256                                             \
+                    -out ${ROOTCA_MOUNT_LOCATION}/rootCA.key            \
+                    -pass file:${ROOTCA_MOUNT_LOCATION}/rootCA_pkey_password
+    chmod 400 ${ROOTCA_MOUNT_LOCATION}/rootCA.key
+
+    openssl req -x509                                                       \
+                -new                                                        \
+                -sha256                                                     \
+                -days 7300                                                  \
+                -subj ${ROOTCA_PEM_SUBJECT}                                 \
+                -key ${ROOTCA_MOUNT_LOCATION}/rootCA.key                    \
+                -passin file:${ROOTCA_MOUNT_LOCATION}/rootCA_pkey_password  \
+                -out ${ROOTCA_MOUNT_LOCATION}/rootCA.pem
+    chmod 444 ${ROOTCA_MOUNT_LOCATION}/rootCA.pem
+
 else
     echo -e "Private key already exists!\nRootCA certificate generation interrupted!";
     exit 10;
 fi
+
+while :; do
+    sleep 1
+done
