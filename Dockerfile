23a6799aee269a54c22cf363b5616e5486cf63f5
fluffehGit <cst.petre@zoho.eu>
2022-09-29 12:11:36 +0300

    +container function switch var

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,24 @@
 FROM alpine:3.16.2
 
 # Base container setup
-RUN apk update && \
-    apk add tzdata && \
+RUN apk update      && \
+    apk add tzdata  && \
     apk add openssl
 
 ENV TZ="Europe/Brussels"
 
 # Create base folders for CA
-RUN mkdir /opt/ca-crate \
-          /opt/ca-crate/certs \
-          /opt/ca-crate/crl \
-          /opt/ca-crate/newcerts \
-          /opt/ca-crate/private \
+RUN mkdir /opt/ca-crate             \
+          /opt/ca-crate/certs       \
+          /opt/ca-crate/crl         \
+          /opt/ca-crate/newcerts    \
+          /opt/ca-crate/private     \
           /opt/ca-crate/CSRs
 
 COPY ./openssl.cnf /etc/ssl/openssl.cnf
 
-RUN touch /opt/ca-crate/index.txt && \
-    echo "1000" > /opt/ca-crate/serial && \
+RUN touch /opt/ca-crate/index.txt       && \
+    echo "1000" > /opt/ca-crate/serial  && \
     chmod 700 /opt/ca-crate/private
 
 COPY ./init.sh /root
--- init.sh
+++ init.sh
@@ -1,31 +1,38 @@
 #!/bin/sh
 
-ROOTCA_MOUNT_LOCATION="/opt/ca-crate/rootCA"
-source ${ROOTCA_MOUNT_LOCATION}/rootCA_pem_subject
+CA_GENERATE=$(echo ${CA_GENERATE} | tr '[:lower:]' '[:upper:]')
 
-if [ ! -e ${ROOTCA_MOUNT_LOCATION}/rootCA.key ]; then
-    openssl genpkey -algorithm RSA                                      \
-                    -pkeyopt rsa_keygen_bits:4096                       \
-                    -aes256                                             \
-                    -out ${ROOTCA_MOUNT_LOCATION}/rootCA.key            \
-                    -pass file:${ROOTCA_MOUNT_LOCATION}/rootCA_pkey_password
-    chmod 400 ${ROOTCA_MOUNT_LOCATION}/rootCA.key
+if [ ${CA_GENERATE} = "ROOT" ]; then
+    ROOTCA_MOUNT_LOCATION="/opt/ca-crate/rootCA"
+    source ${ROOTCA_MOUNT_LOCATION}/rootCA_pem_subject
 
-    openssl req -x509                                                       \
-                -new                                                        \
-                -sha256                                                     \
-                -days 7300                                                  \
-                -subj ${ROOTCA_PEM_SUBJECT}                                 \
-                -key ${ROOTCA_MOUNT_LOCATION}/rootCA.key                    \
-                -passin file:${ROOTCA_MOUNT_LOCATION}/rootCA_pkey_password  \
-                -out ${ROOTCA_MOUNT_LOCATION}/rootCA.pem
-    chmod 444 ${ROOTCA_MOUNT_LOCATION}/rootCA.pem
+    if [ ! -e ${ROOTCA_MOUNT_LOCATION}/rootCA.key ]; then
+        openssl genpkey -algorithm RSA                                      \
+                        -pkeyopt rsa_keygen_bits:4096                       \
+                        -aes256                                             \
+                        -out ${ROOTCA_MOUNT_LOCATION}/rootCA.key            \
+                        -pass file:${ROOTCA_MOUNT_LOCATION}/rootCA_pkey_password;
+        chmod 400 ${ROOTCA_MOUNT_LOCATION}/rootCA.key;
 
+        openssl req -x509                                                       \
+                    -new                                                        \
+                    -sha256                                                     \
+                    -days 7300                                                  \
+                    -subj ${ROOTCA_PEM_SUBJECT}                                 \
+                    -key ${ROOTCA_MOUNT_LOCATION}/rootCA.key                    \
+                    -passin file:${ROOTCA_MOUNT_LOCATION}/rootCA_pkey_password  \
+                    -out ${ROOTCA_MOUNT_LOCATION}/rootCA.pem;
+        chmod 444 ${ROOTCA_MOUNT_LOCATION}/rootCA.pem;
+
+    else
+        echo -e "Private key already exists!\nRootCA certificate generation interrupted!";
+        exit 10;
+    fi
+
+elif [ ${CA_GENERATE} = "CLIENT" ]; then
+    echo "TBD: Generate clients functionality";
+    exit 1;
 else
-    echo -e "Private key already exists!\nRootCA certificate generation interrupted!";
-    exit 10;
+    echo "Option ${CA_GENERATE} is not valid!"
+    exit 11;
 fi
-
-while :; do
-    sleep 1
-done
